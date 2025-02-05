516e14a2fd3abc704d533ece9c192fc7086c942c
aerin <92151406+aerinxo@users.noreply.github.com>
2025-02-05 22:59:44 +0530

    chngs

---
+++ src/sections/LogoTicker.tsx
@@ -0,0 +1,119 @@
+'use client';
+import { LogoCloud } from '@/components/LogoCloud';
+import logo1 from "@/assets/python-logo.png";
+import logo2 from "@/assets/raspberry-logo.png";
+import logo3 from "@/assets/opencv-logo.png";
+
+
+export const LogoTicker = () => {
+  return (
+     <section
+      id="clients"
+      className="mx-auto max-w-4xl px-6 text-center md:px-8"
+    >
+      <div className="py-14">
+        <div className="mx-auto max-w-screen-xl px-4 md:px-8">
+          <h2 className="text-center text-lg  font-medium text-white/70">
+            Trusted by Teams Around the World
+          </h2>
+          <div className="mt-12">
+            
+          <LogoCloud
+  limit={6}
+  logos={[
+    {
+      href: "https://enroll.dopler.app",
+      icon: false,
+      size: "xs",
+      wordmarkSrc: "/assets/trademark/apex.png",
+    },
+    {
+      href: "https://dopler.app",
+      icon: false,
+      size: "xs",
+      wordmarkSrc: "/assets/trademark/apex.png",
+    },
+    {
+      href: "https://club.dopler.io",
+      icon: false,
+      size: "xs",
+      wordmarkSrc: "/assets/trademark/apex.png",
+    },
+    {
+      href: "https://magic-portfolio.com",
+      icon: false,
+      size: "xs",
+      wordmarkSrc: "/assets/trademark/apex.png",
+    },
+    {
+      href: "https://enroll.dopler.app",
+      icon: false,
+      size: "xs",
+      wordmarkSrc: "/assets/trademark/apex.png",
+    },
+    {
+      href: "https://dopler.app",
+      icon: false,
+      size: "xs",
+      wordmarkSrc: "/assets/trademark/apex.png",
+    },
+    {
+      href: "https://enroll.dopler.app",
+      icon: false,
+      size: "xs",
+      wordmarkSrc: "/assets/trademark/apex.png",
+    },
+    {
+      href: "https://dopler.app",
+      icon: false,
+      size: "xs",
+      wordmarkSrc: "/assets/trademark/apex.png",
+    },
+    {
+      href: "https://enroll.dopler.app",
+      icon: false,
+      size: "xs",
+      wordmarkSrc: "/assets/trademark/apex.png",
+    },
+    {
+      href: "https://dopler.app",
+      icon: false,
+      size: "xs",
+      wordmarkSrc: "/assets/trademark/apex.png",
+    },
+    {
+      href: "https://club.dopler.io",
+      icon: false,
+      size: "xs",
+      wordmarkSrc: "/assets/trademark/apex.png",
+    },
+    {
+      href: "https://magic-portfolio.com",
+      icon: false,
+      size: "xs",
+      wordmarkSrc: "/assets/trademark/apex.png",
+    },
+    {
+      href: "https://enroll.dopler.app",
+      icon: false,
+      size: "xs",
+      wordmarkSrc: "/assets/trademark/apex.png",
+    },
+    {
+      href: "https://dopler.app",
+      icon: false,
+      size: "xs",
+      wordmarkSrc: "/assets/trademark/apex.png",
+    },
+  ]}
+  columns={3} // Keep 2 columns
+  mobileColumns={1} // Stack on mobile
+/>
+
+          </div>
+        </div>
+      </div>
+    </section>
+
+  );
+};
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,18 +1,54 @@
 import type { Config } from "tailwindcss";
 
-export default {
+
+const defaultTheme = require("tailwindcss/defaultTheme");
+const colors = require("tailwindcss/colors");
+const {
+  default: flattenColorPalette,
+} = require("tailwindcss/lib/util/flattenColorPalette");
+
+const config: Config = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/sections/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: "20px",
+        lg: "80px",
+      },
+      screens: {
+        sm: "375px",
+        md: "768px",
+        lg: "1200px",
       },
     },
+    screens: {
+      sm: "375px",
+      md: "768px",
+      lg: "1200px",
+    },
+    extend: {},
   },
-  plugins: [],
-} satisfies Config;
+  plugins: [addVariablesForColors ],
+
+
+  
+};
+
+
+function addVariablesForColors({ addBase, theme }: any) {
+  let allColors = flattenColorPalette(theme("colors"));
+  let newVars = Object.fromEntries(
+    Object.entries(allColors).map(([key, val]) => [`--${key}`, val])
+  );
+ 
+  addBase({
+    ":root": newVars,
+  });
+}
+
+export default config;
