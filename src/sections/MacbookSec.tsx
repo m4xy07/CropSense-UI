0a201ee13580d55d1f1d09e9f267e3332ce6be4f
aerin <aman.10shaikh@gmail.com>
2025-02-08 23:24:29 +0530

    fixes

--- src/sections/MacbookSec.tsx
+++ src/sections/MacbookSec.tsx
@@ -7,7 +7,7 @@ import Page from "@/app/dashboard/page";
 const MacbookSec = () => {
   return (
     
-    <section id="team" className="pb-10 mb-40 pt-10 lg:pb-20">
+    <section id="team" className="pb-10 mb-80 pt-10 lg:pb-20">
       <LampDemo/>
       <div className="container -mt-48 mx-auto">
         <MacbookScrollDemo/>
