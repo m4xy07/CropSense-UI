b19fb381956a41cff344b997b275da718eb61e18
aerin <aman.10shaikh@gmail.com>
2025-02-08 18:44:24 +0530

    fixes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,8 +6,8 @@ import { twMerge } from "tailwind-merge";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "EcoCred",
-  description: "A landing page for an AI startup created with Frontend Tribe",
+  title: "CropSense",
+  description: "Tech So Bright, Crops Grow Right!",
 };
 
 export default function RootLayout({
