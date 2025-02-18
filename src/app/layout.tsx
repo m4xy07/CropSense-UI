025573c06b954e9f3439473822c73aab54327830
aerin <aman.10shaikh@gmail.com>
2025-02-18 19:57:05 +0530

    clerk setup

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { twMerge } from "tailwind-merge";
+import { ClerkProvider } from "@clerk/nextjs";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,9 +18,25 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <ClerkProvider
+      appearance={{
+        layout:{
+          logoImageUrl: '/Logo_Rev_1_Transparent.png',
+          socialButtonsVariant: 'iconButton'
+        },
+        variables: {
+          colorText: '#fff',
+          colorPrimary: '#0E78F9',
+          colorBackground: '#1c1f2e',
+          colorInputBackground: '#252a41',
+          colorInputText: '#fff'
+        }
+      }}
+      >
       <body className={twMerge(inter.className, "bg-black text-white antialiased")}>
         {children}
         </body>
+        </ClerkProvider>
       
     </html>
   );
---
+++ src/app/login/page.tsx
@@ -0,0 +1,18 @@
+"use client";
+import Footerdemo from '@/sections/Footer';
+import { Header } from '@/sections/Header'
+import React from 'react'
+
+const loginpage = () => {
+  return (
+    <>
+    <Header />
+    <div className='bg-black min-h-[90vh] text-white items-center justify-center flex flex-col'>
+      oke
+    </div>
+    <Footerdemo />
+    </>
+  )
+}
+
+export default loginpage
