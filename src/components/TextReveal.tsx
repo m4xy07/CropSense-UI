5cc1c4314e715a66e3b33bf88e8b9f6a626b69ba
aerin <92151406+aerinxo@users.noreply.github.com>
2025-04-21 12:37:12 +0530

    Update TextReveal.tsx

--- src/components/TextReveal.tsx
+++ src/components/TextReveal.tsx
@@ -31,7 +31,7 @@ export const TextReveal: FC<TextRevealProps> = ({ children, className }) => {
         <span
           ref={targetRef}
           className={
-            "flex flex-wrap p-5 text-2xl  font-bold text-black/20 dark:text-white/20 md:p-8 md:text-3xl lg:p-10 lg:text-4xl xl:text-5xl"
+            "flex flex-wrap p-5 text-2xl  font-bold text-white/20 md:p-8 md:text-3xl lg:p-10 lg:text-4xl xl:text-5xl"
           }
         >
           {words.map((word, i) => {
@@ -62,7 +62,7 @@ const Word: FC<WordProps> = ({ children, progress, range }) => {
       <span className="absolute opacity-30 ">{children}</span>
       <motion.span
         style={{ opacity: opacity }}
-        className={"text-black  dark:text-white"}
+        className={"text-white"}
       >
         {children}
       </motion.span>
