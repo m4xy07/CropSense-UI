26e468d2403620a18bc1938c9a1aee8eddb9bdfc
Maximus <aman.10shaikh@gmail.com>
2025-06-02 10:25:30 +0530

    chngs

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -102,7 +102,7 @@ export default function Page() {
               </Breadcrumb>
             </div>
             <div className="flex flex-row gap-2 items-center">
-              <ToastComponent />
+              {/* <ToastComponent /> */}
               <div className="relative hover:bg-[rgba(255,255,255,0.025)] group p-[6px] rounded-md flex flex-row items-center cursor-pointer transition-all duration-200 ease-in-out">
                 <div className="h-[10px] w-[10px] rounded-full bg-[#f4af29] alert-animation absolute top-0 right-0" />
                 <Bell className="w-[18px] h-[18px] text-[rgba(255,255,255,.75)] group-hover:text-[#fff] transition-all duration-200 ease-in-out" />
