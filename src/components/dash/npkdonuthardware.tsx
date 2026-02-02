e65e9ec8eb492d5303d0bca5bcb4ccb5c551d63e
Aman Shaikh <hey@m4xy.org>
2026-02-02 09:37:28 +0530

    Update npkdonuthardware.tsx

--- src/components/dash/npkdonuthardware.tsx
+++ src/components/dash/npkdonuthardware.tsx
@@ -1,15 +1,11 @@
-"use client"
+"use client";
 
-import { useEffect, useState } from "react"
-import { TrendingUp } from "lucide-react"
-import { Pie, PieChart } from "recharts"
+import { useEffect, useState } from "react";
+import { TrendingUp } from "lucide-react";
+import { Pie, PieChart } from "recharts";
 import Button from "../Button";
 
-import {
-  Card,
-  CardContent,
-  CardFooter,
-} from "@/components/ui/card"
+import { Card, CardContent, CardFooter } from "@/components/ui/card";
 
 import {
   ChartConfig,
@@ -18,47 +14,47 @@ import {
   ChartTooltipContent,
   ChartLegend,
   ChartLegendContent,
-} from "@/components/ui/chart"
+} from "@/components/ui/chart";
 import NPKTableComponent from "../dashboard/linedtable";
 import Link from "next/link";
 
 interface NPKData {
-  npk_uptake_nitrogen: number
-  npk_uptake_phosphorus: number
-  npk_uptake_potassium: number
+  npk_uptake_nitrogen: number;
+  npk_uptake_phosphorus: number;
+  npk_uptake_potassium: number;
 }
 
 export function NPKDonutComponentHardware() {
-  const [chartData, setChartData] = useState<any[]>([])
+  const [chartData, setChartData] = useState<any[]>([]);
 
   useEffect(() => {
     // Placeholder data matching /dashboard
     const transformedData = [
-    {
+      {
         label: "Nitrogen",
-        value: 12,
+        value: 7,
         fill: "#d7721a", // Cyan
-    },
-    {
+      },
+      {
         label: "Phosphorus",
         value: 2,
         fill: "#d52c9e", // Orange
-    },
-    {
+      },
+      {
         label: "Potassium",
-        value: 6,
+        value: 4,
         fill: "#974ae5", // Purple
-    },
-    ]
-    setChartData(transformedData)
-  }, [])
+      },
+    ];
+    setChartData(transformedData);
+  }, []);
 
   const chartConfig: ChartConfig = {
-  value: { label: "Uptake" },
-  Nitrogen: { label: "Nitrogen", color: "#d7721a" },
-  Phosphorus: { label: "Phosphorus", color: "#d52c9e" },
-  Potassium: { label: "Potassium", color: "#974ae5" },
-}
+    value: { label: "Uptake" },
+    Nitrogen: { label: "Nitrogen", color: "#d7721a" },
+    Phosphorus: { label: "Phosphorus", color: "#d52c9e" },
+    Potassium: { label: "Potassium", color: "#974ae5" },
+  };
 
   return (
     <Card className="flex flex-col border border-zinc-50/10 rounded-xl equipment-card-inner w-full items-start p-0">
@@ -94,11 +90,15 @@ export function NPKDonutComponentHardware() {
           <NPKTableComponent />
           <div className="flex flex-row items-center justify-center gap-2 py-2 px-4 border border-zinc-50/10 alert-dashboard-theme rounded-md">
             <div className="h-[10px] w-[10px] rounded-full bg-[#d10412] warning-animation" />
-            <span className="text-[13px]">Deficiency detected! Take action now to avoid issues.</span>
+            <span className="text-[13px]">
+              Deficiency detected! Take action now to avoid issues.
+            </span>
           </div>
-          <Link href="https://dir.indiamart.com/impcat/npk-fertilizer/npk-ratio-15-15-15-q15410334/"><Button>Order NPK 15-15-15</Button></Link>
+          <Link href="https://dir.indiamart.com/impcat/npk-fertilizer/npk-ratio-15-15-15-q15410334/">
+            <Button>Order NPK 15-15-15</Button>
+          </Link>
         </div>
       </div>
     </Card>
-  )
+  );
 }
